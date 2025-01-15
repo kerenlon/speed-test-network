32547414f901fceb0c16b2c70138fee1bde84e14
Freac231 <livshmic@post.bgu.ac.il>
2025-01-15 22:11:06 +0200

    Add files via upload

---
+++ my_client.py
@@ -0,0 +1,5 @@
+from Client import Client
+
+if __name__ == '__main__':
+    my_client = Client()
+    my_client.start()
---
+++ my_server.py
@@ -0,0 +1,9 @@
+from Server import Server
+
+if __name__ == '__main__':
+    my_server = Server()
+    my_server.start()
+
+
+
+
